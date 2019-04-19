9cf9eb401487c7820723dbf7593dbce600db39f0
Syd Amir <hemedani@gmail.com>
2019-04-19 14:14:39 +0430

    just remane inspection

--- models/inspection.js
+++ models/inspection.js
@@ -4,13 +4,7 @@ const Schema = mongoose.Schema;
 const inspectionSchema = new Schema(
   {
     detail: String,
-    seenBy: [
-      {
-        _id: Schema.Types.ObjectId,
-        name: String,
-        count: Number
-      }
-    ],
+    seenBy: [{ _id: Schema.Types.ObjectId, name: String, count: Number }],
     center: { type: Schema.Types.ObjectId, ref: "Center" },
     creator: { type: Schema.Types.ObjectId, ref: "User" }
   },
