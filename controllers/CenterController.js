351b1ff0ba0cd85d22e44c47daed6be0fc2450d3
Syd Amir <hemedani@gmail.com>
2019-06-10 19:29:56 +0430

    change static map to yandex

--- controllers/CenterController.js
+++ controllers/CenterController.js
@@ -13,17 +13,32 @@ const exRate = require("../service/exRate").exRate;
 const download = require("image-downloader");
 const uuidv4 = require("uuid/v4");
 
+// TODO whenever i can i want to change static map to open street map ==================
 const StaticMapApi = "AIzaSyCxOHHEJ0dio2EAIAxeKzUnPimk7GNJpn0";
+
+// https://static-maps.yandex.ru/1.x/?lang=en-US&ll=32.810152,39.889847&%20\%20size=450,450&z=16&l=map&pt=32.810152,39.889847,pm2rdl
+const YandexStaticMapUrl = (lat, lng) =>
+  `https://static-maps.yandex.ru/1.x/?ll=${lng},${lat}&%20\%20size=450,450&z=16&l=map&pt=${lng},${lat},pm2rdl`;
+
+const GoogleStaticMapUrl = (lat, lng) =>
+  `https://maps.googleapis.com/maps/api/staticmap?language=fa&center=${lat},${lng}&zoom=16&size=640x400&maptype=roadmap&markers=icon:https://pasteboard.co/IagJJEM.png%7C${lat},${lng}&key=${StaticMapApi}`;
 
 const updateStaticMap = async (lat, lng) => {
   const staticMapImgName = `${uuidv4()}.png`;
 
   const mapOptions = {
-    url: `https://maps.googleapis.com/maps/api/staticmap?language=fa&center=${lat},${lng}&zoom=16&size=640x400&maptype=roadmap&markers=icon:https://pasteboard.co/IagJJEM.png%7C${lat},${lng}&key=${StaticMapApi}`,
+    url: YandexStaticMapUrl(lat, lng),
     dest: `./pic/maps/${staticMapImgName}`
   };
 
-  await download.image(mapOptions);
+  try {
+    await download.image(mapOptions);
+  } catch (error) {
+    console.log("==================");
+    console.log("error", error);
+    console.log("==================");
+  }
+
   return staticMapImgName;
 };
 
